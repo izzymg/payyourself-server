b41cbfbec0045ee371617ba1cd41122ee106c7d0
izzymg <izzymhg@gmail.com>
2021-06-28 10:53:19 +1200

    Remove unused dev script

--- serve-min-client.js
+++
@@ -1,9 +0,0 @@
-const http = require("http")
-const fs = require("fs")
-
-const server = http.createServer((req, res) => {
-    const stream = fs.createReadStream("./min-client/index.html")
-    stream.pipe(res, { end: true })
-})
-
-server.listen(3000, "0.0.0.0")
