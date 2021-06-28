76e5fbcbf4c77eafedd6854176c2914b3be1c4c2
izzymg <izzymhg@gmail.com>
2021-06-28 18:57:20 +1200

    Remove logging from storage module

--- server/storage/google.go
+++ server/storage/google.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"io"
-	"log"
 	"py-server/server"
 
 	"cloud.google.com/go/storage"
@@ -16,18 +15,14 @@ type GoogleStorer struct {
 }
 
 func (gs GoogleStorer) Fetch(userID string) (io.ReadCloser, error) {
-	log.Printf("fetching google usersave for %s", userID)
-
 	reader, err := gs.client.Bucket("user-saves-1").Object(userID).NewReader(context.TODO())
 	if err != nil {
-		log.Printf("fetching google usersave failed %s", err)
 		if errors.Is(err, storage.ErrObjectNotExist) {
 			return nil, server.ErrNoUserSave
 		} else {
 			return nil, err
 		}
 	}
-	log.Printf("fetched google usersave for %s", userID)
 	return reader, nil
 }
 
@@ -36,12 +31,10 @@ func (gs GoogleStorer) Close() error {
 }
 
 func MakeGoogleStorer(ctx context.Context) (*GoogleStorer, error) {
-	log.Printf("creating google storage cloud")
 	client, err := storage.NewClient(ctx)
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("created google storage cloud")
 	return &GoogleStorer{
 		client,
 	}, nil
