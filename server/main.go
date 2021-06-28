01ca102986bf29e64e98c941fd7bb9e0d21b2b8b
izzymg <izzymhg@gmail.com>
2021-06-28 16:34:26 +1200

    Add main.go entry point

--- server/main.go
+++ server/main.go
@@ -1,4 +1,60 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"py-server/server"
+	"py-server/storage"
+	"py-server/token"
+)
+
+func isDevMode() bool {
+	devFlag := flag.Bool("development", false, "Runs the server in development mode")
+	flag.Parse()
+	return *devFlag
+}
+
+func getServerAddr() string {
+	addr, found := os.LookupEnv("PYSERVER_ADDR")
+	if !found {
+		return "0.0.0.0:5000"
+	}
+	return addr
+}
+
 func main() {
+	ctx := context.Background()
+	shutdownServer := make(chan error)
+
+	storerRoot := os.Getenv("PYSERVER_STORE_ROOT")
+	if len(storerRoot) == 0 {
+		log.Fatal("no store root provided")
+	}
+
+	serverAddr := getServerAddr()
+	checkerClientID, foundClientID := os.LookupEnv("PYSERVER_CLIENTID")
+	if !foundClientID && !isDevMode() {
+		log.Fatal("client ID must be provided if server is not in development mode")
+	}
+
+	storer, err := storage.MakeFileSystemStorer(storerRoot)
+	if err != nil {
+		log.Fatalf("failed to make storer: %s", err)
+	}
+
+	routeHandlers := server.AppRouteHandlers{
+		UserSaveStorer: storer,
+		TokenChecker:   token.MakeGoogleTokenChecker(checkerClientID),
+	}
+	go server.Serve(ctx, serverAddr, shutdownServer, server.Route(routeHandlers))
+
+	log.Println(fmt.Sprintf("server started on %s", serverAddr))
+	err = <-shutdownServer
+	log.Println("exiting")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
